31143b3c46126fedfced7d95c9e6e56fdfb712e2
Chris Castle <crcastle@gmail.com>
2012-02-20 14:53:23 -0800

    change from json signing to string signing

--- readme.md
+++ readme.md
@@ -1,16 +1,10 @@
-signed-request [![Build Status](https://secure.travis-ci.org/nshah/nodejs-signed-request.png)](http://travis-ci.org/nshah/nodejs-signed-request)
+signed-request
 ==============
 
-A signed JSON container. The form is basically:
+A node.js signed string container. The form is basically:
 
 ```javascript
-base64url(hmac256(json(data))).base64url(json(data))
+base64url(hmac256(string)).base64url(string)
 ```
 
-That's a base64 URL encoded hmac256 hash of the JSON representation of the
-data, followed by a dot `.`, followed by the base64 URL encoded JSON
-representation itself.
-
-This is the same as the
-[Facebook `signed_request`](https://developers.facebook.com/docs/authentication/signed_request/).
-But it's generically useful.
+That's a base64 URL encoded hmac256 hash of the string, followed by an ampersand `&`, followed by the base64 URL encoded string representation itself.
--- test/signed-request-test.js
+++ test/signed-request-test.js
@@ -1,22 +1,26 @@
-var signedRequest = require('signed-request')
-  , assert = require('assert')
+var signedRequest = require('signed-request'),
+    assert = require('assert');
 
+/*
 function isInt(n) {
-  return typeof n == 'number' && n % 1 === 0
+  return typeof n == 'number' && n % 1 === 0;
 }
+*/
 
 exports['sign and verify'] = function() {
-  var data = { answer: 42 }
-    , secret = 'abcd'
-    , signed = signedRequest.stringify(data, secret)
-    , parsed = signedRequest.parse(signed, secret, 1000)
-  assert.equal(data.answer, parsed.answer, 'expect the answer.')
-}
+  var data = 'this is a secret message!',
+      secret = 'abcd',
+      signed = signedRequest.stringify(data, secret),
+      parsed = signedRequest.parse(signed, secret);
+  assert.equal(data, parsed, 'expect the answer.');
+};
 
+/*
 exports['issued_at is an integer'] = function() {
-  var data = { answer: 42 }
-    , secret = 'abcd'
-    , signed = signedRequest.stringify(data, secret)
-    , parsed = signedRequest.parse(signed, secret, 1000)
-  assert(isInt(parsed.issued_at))
-}
+  var data = { answer: 42 },
+      secret = 'abcd',
+      signed = signedRequest.stringify(data, secret),
+      parsed = signedRequest.parse(signed, secret, 1000);
+  assert(isInt(parsed.issued_at));
+};
+*/
